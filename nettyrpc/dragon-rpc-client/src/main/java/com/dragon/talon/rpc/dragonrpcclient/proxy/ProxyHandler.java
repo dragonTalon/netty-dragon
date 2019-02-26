65e7f4edddd4676d52dabd0d52628013a688fb0e
蹇轶凡 <wuzhu.jyf@raycloud.com>
2019-02-26 11:11:01 +0800

    重复使用channel 不然的话每次建立一个tcp连接，会导致连接非常大

--- nettyrpc/dragon-rpc-client/src/main/java/com/dragon/talon/rpc/dragonrpcclient/proxy/ProxyHandler.java
+++ nettyrpc/dragon-rpc-client/src/main/java/com/dragon/talon/rpc/dragonrpcclient/proxy/ProxyHandler.java
@@ -4,11 +4,13 @@ import com.dragon.talon.rpc.dragonrpcclient.netty.TcpClient;
 import com.dragon.talon.rpc.dragonrpccommon.serializer.SerializerUtils;
 import com.dragon.talon.rpc.dragonrpccommon.transfor.Request;
 import com.dragon.talon.rpc.dragonrpccommon.transfor.Response;
+import com.google.common.collect.Maps;
 import io.netty.channel.ChannelFuture;
 import org.springframework.cglib.proxy.InvocationHandler;
 
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.Map;
 import java.util.Objects;
 
 /**
@@ -17,28 +19,34 @@ import java.util.Objects;
  * @author dragonboy
  */
 public class ProxyHandler implements InvocationHandler {
-    
+
     private String address;
-    
+
     private int port;
     
+    private static final Map<Class<?>, TcpClient> channelMap = Maps.newConcurrentMap();
+
     @Override
     public Object invoke(Object o, Method method, Object[] objects) throws Throwable {
         Request request = Request.newInstance(method, o.getClass().getName(), objects);
-        TcpClient tcpClient = new TcpClient(address,port);
+        Class<?> declaringClass = method.getDeclaringClass();
+        TcpClient tcpClient = channelMap.get(declaringClass);
+        if (Objects.isNull(tcpClient)) {
+            tcpClient = new TcpClient(address, port);
+        }
         tcpClient.sendMsg(request);
         Response response = tcpClient.getResponse(request.getRequestId(), 10000);
         Class<?> returnType = method.getReturnType();
         Object obj = response.getObject();
-        return getResult(obj,returnType);
+        return getResult(obj, returnType);
     }
-    
-    public Object bind(Class<?>[] interfaces,String address,int port){
+
+    public Object bind(Class<?>[] interfaces, String address, int port) {
         this.address = address;
         this.port = port;
-        return Proxy.newProxyInstance(ClassLoader.getSystemClassLoader(),interfaces,this::invoke);
+        return Proxy.newProxyInstance(ClassLoader.getSystemClassLoader(), interfaces, this::invoke);
     }
-    
+
     private <T> T getResult(Object obj, Class<T> clazz) {
         return (T) obj;
     }
